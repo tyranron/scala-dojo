c60a2c01bd6f19c4784630943906386f38b2222f
tyranron <tyranron@gmail.com>
2021-12-05 22:16:21 +0100

    [fpinscala] Complete exercise 2.4

---
+++ src/test/scala/fpinscala/Exercise_2_4.scala
@@ -0,0 +1,11 @@
+import me.tyranron.fpinscala.Exercise_2_4.uncurry
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class Exercise_2_4 extends AnyFlatSpec {
+  "uncurry" should "work" in {
+    val add = uncurry((x: Int) => (y: Int) => x + y)
+
+    assert(add(3, 4) == 7)
+  }
+}
